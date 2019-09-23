f0d765eddb94b05a96bedfd61cd93b1a8b25c9d9
Sophie Kunz <sophie.kunz@qualitasag.ch>
2019-09-23 16:28:55 +0200

    Step 1: For each traits&random_effect get the mean value of all variants

--- R/ConstructingMultiEst.R
+++ R/ConstructingMultiEst.R
@@ -11,14 +11,15 @@ library(dplyr)
 ### # Read all VCE results
 s_vce_result <- "../../work/VCE_resluts.csv"
 tbl_vce <- readr::read_delim(file = s_vce_result, delim = ";")
-### # Only Sample 1 da meistens status 1
-tbl_vce <- tbl_vce %>% filter(data_subset_number == "Sample_1")
+tbl_vce$estimate <- as.numeric(as.character(tbl_vce$estimate))
 
 ### # 'Matrices: NATRUAL' are the inputs for mix99 in the routine evaluation containing variance and covariance
-tbl_varCovar <- tbl_vce %>% filter(type == "variance" | type == "covariance") %>% select(type,traits,random_effect,estimate)
+tbl_varCovar <- tbl_vce %>% filter(type == "variance" | type == "covariance") %>% select(type,traits,random_effect,estimate,trait_combination)
 
 
 ### # Build Matrice with Ccc Cca Cfc Cfa Cwc Cwa Cac Caa
+### # Step 1: For each traits&random_effect get the mean value of all variants
 by_grp <- tbl_varCovar %>% group_by(traits, random_effect)
 smry <- summarise(by_grp,
-                  meanEstimate = mean(estimate))
+                  meanEstimate = mean(estimate, na.rm = TRUE))
+
